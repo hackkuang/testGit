58f4bcdb2148c4cfe1a2ca7795998b5d8e7746d5
kuangmin <kuangmin@coshine.com>
2019-03-25 11:07:04 +0800

    no message

--- app/src/main/java/com/example/myapplication/units/NetWorkUtils.java
+++ app/src/main/java/com/example/myapplication/units/NetWorkUtils.java
@@ -16,8 +16,10 @@ public class NetWorkUtils {
         LocationManager locationManager
                 = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         // 通过GPS卫星定位，定位级别可以精确到街（通过24颗卫星定位，在室外和空旷的地方定位准确、速度快）
-        assert locationManager != null;
-        boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        //assert locationManager != null;
+
+       // boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean gps = false;
         // 通过WLAN或移动网络(3G/2G)确定的位置（也称作AGPS，辅助GPS定位。主要用于在室内或遮盖物（建筑群或茂密的深林等）密集的地方定位）
         boolean network = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
         return gps || network;
